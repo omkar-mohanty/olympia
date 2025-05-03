1cbdccf808f4aa9063a657de65efa392713d5ba4
Omkr <franzohouser@gmail.com>
2025-05-03 23:19:12 +0530

    gui : Compile GUI

--- olympia-cli/src/main.rs
+++ olympia-cli/src/main.rs
@@ -1,16 +1,18 @@
 use std::path::PathBuf;
 
-use olympia_core::load;
 use clap::Parser;
+use olympia_core::load;
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
-    path: PathBuf
+    path: PathBuf,
 }
 
 fn main() {
-    env_logger::builder().filter_level(log::LevelFilter::Info).init();
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     let cli = Cli::parse();
     let _ = load(cli.path.as_path());
 }
